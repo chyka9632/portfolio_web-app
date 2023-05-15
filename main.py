e8a99b4f039225fc4a2b0bb782a9814318ba79c9
Chika <chyka9632@gmail.com>
2023-05-15 22:31:56 +0100

    Render the app title in two columns

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
+import pandas as pd
 import streamlit as st
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
+
 with col1:
-    st.image("images/photo.jpeg")
+    st.image("images/photo1.jpeg")
+
 
 with col2:
     st.title("Chika Uchendu")
     with open("profile.txt", "r") as file:
         content = file.read()
 
-    st.write(content)
+    st.info(content)
+
+content1 = """ Check out my recent Python-built apps.
+Feel free to reach out!
+"""
+st.write(content1)
+
+col3, col4 = st.columns(2)
+
+df = pd.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
